9dbea5729df254f1fac0a1e8558b66809bdeda5b
Marcin Smola <marcinure1@gmail.com>
2025-10-02 01:50:39 +0200

    Fix typo

--- build.zig
+++ build.zig
@@ -64,9 +64,11 @@ pub fn build(b: *std.Build) void {
     test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
     if (builtin.os.tag == .windows) {
-        // const password = std.process.getenvW("PASSWORD");
-        const sign_cmd = b.addSystemCommand(&.{"signtool", "sign", "/a", "/fd", "SHA256", exe.installed_path});
-        const sign_step = b.step("sign", "Sign windows exe");
-        sign_step.dependOn(&sign_cmd.step);
+        if(exe.installed_path) |path| {
+            // const password = std.process.getenvW("PASSWORD");
+            const sign_cmd = b.addSystemCommand(&.{"signtool", "sign", "/a", "/fd", "SHA256", path});
+            const sign_step = b.step("sign", "Sign windows exe");
+            sign_step.dependOn(&sign_cmd.step);
+        }
     }
 }
